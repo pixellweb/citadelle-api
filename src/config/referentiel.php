2e57616b1b3ec3cfc924504ef9a5189cb5939e89
B1nj <postmaster@b1nj.fr>
2021-06-22 17:40:24 -0400

    lifestyle

--- src/config/referentiel.php
+++ src/config/referentiel.php
@@ -12,7 +12,8 @@ return [
             'energy' => \App\Models\Vehicule\Referentiel\Energie::class,
             'transmission' => \App\Models\Vehicule\Referentiel\Transmission::class,
             'category' => \App\Models\Vehicule\Referentiel\Categorie::class,
-            /*'couleur' => \App\Models\Vehicule\Referentiel\Couleur::class,*/
+            /*'lifestyle' => \App\Models\Vehicule\Referentiel\Lifestyle::class,
+            'couleur' => \App\Models\Vehicule\Referentiel\Couleur::class,*/
             'marque' => \App\Models\Vehicule\Referentiel\Marque::class,
             'modele' => \App\Models\Vehicule\Referentiel\Modele::class,
             'caracteristique' => \App\Models\Vehicule\Referentiel\Caracteristique::class,
